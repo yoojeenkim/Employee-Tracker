5d15c0d231bc3962aa39e8222df92cbe6b2c661f
daniel <yoojeenkim@gmail.com>
2021-10-14 22:19:19 -0400

    starting files

---
+++ server.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection();
