6093193155068984d614b5bc8556f2dd4fe83742
daniel <yoojeenkim@gmail.com>
2021-10-16 20:37:03 -0400

    added foreign keys to schema

--- db/schema.sql
+++ db/schema.sql
@@ -11,13 +11,22 @@ CREATE TABLE jobrole (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 )
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    jobrole_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (jobrole_id)
+    REFERENCES jobrole(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 )
--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+// const express = require('express');
+// const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const choices = ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Department', 'Add Department', 'Quit'];
 
-const db = mysql.createConnection();
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// const db = mysql.createConnection(
+//     {
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'Root123',
+//         database: 'company_db'
+//     },
+//     console.log(`Connected to the company_db database`)
+// );
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: choices,
+            name: 'list'
+        }
+    ])
