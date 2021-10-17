f5386197d20c867b03ebdf4af7b2387fb59e2bde
daniel <yoojeenkim@gmail.com>
2021-10-17 01:53:23 -0400

    added queries for roles and departments

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,36 @@
+DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL
+CREATE TABLE departments (
+    id INT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE jobrole (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roles (
+    id INT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES departments(id)
     ON DELETE SET NULL
 )
 
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    jobrole_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    FOREIGN KEY (jobrole_id)
-    REFERENCES jobrole(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
-)
+-- CREATE TABLE employees (
+--     id INT,
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
+--     role_id INT,
+--     manager_id INT,
+--     PRIMARY KEY (id),
+--     FOREIGN KEY (role_id)
+--     REFERENCES roles(id)
+--     ON DELETE SET NULL,
+--     FOREIGN KEY (manager_id)
+--     REFERENCES employees(id)
+--     ON DELETE SET NULL
+-- )
--- server.js
+++ server.js
@@ -1,31 +1,51 @@
 const inquirer = require('inquirer');
-// const express = require('express');
-// const mysql = require('mysql2');
+const express = require('express');
+const mysql = require('mysql2');
 
-// const PORT = process.env.PORT || 3001;
-// const app = express();
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-const choices = ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Department', 'Add Department', 'Quit'];
+// const choices = ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Department', 'Add Department', 'Quit'];
 
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-// const db = mysql.createConnection(
-//     {
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'Root123',
-//         database: 'company_db'
-//     },
-//     console.log(`Connected to the company_db database`)
-// );
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Root123',
+        database: 'company_db'
+    },
+    console.log(`Connected to the company_db database`)
+);
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: choices,
-            name: 'list'
-        }
-    ])
+// db.query('SELECT * FROM employee', function (err, results) {
+//     console.log(results);
+// });
+
+db.query('SELECT * FROM roles', function (err, results) {
+    console.log(results);
+});
+
+db.query('SELECT * FROM departments', function (err, results) {
+    console.log(results);
+});
+
+// inquirer
+//     .prompt([
+//         {
+//             type: 'list',
+//             message: 'What would you like to do?',
+//             choices: choices,
+//             name: 'list'
+//         }
+//     ])
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
