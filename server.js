264d22095e3b389433fca07278d79f0fe0b90dae
daniel <yoojeenkim@gmail.com>
2021-10-18 19:04:22 -0400

    deleted inquire file, moved inquirer to root file, and got rid of express did not know why i included in the first place

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
-const express = require('express');
+const inquirer = require('inquirer');
+const Queries = require('./queries.js');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const choices = ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Department', 'Add Department', 'Quit'];
+const perform = new Queries;
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: choices,
+                name: 'choice'
+            }
+        ])
+        .then((res) => {
+            switch(res.choice) {
+                case 'View All Employees':
+                    perform.viewEmployees();
+                    break;
+                case 'Add Employee':
+                    break;
+                case 'Update Employee Role':
+                    break;
+                case 'View All Roles':
+                    perform.viewRoles();
+                    break;
+                case 'Add Role':
+                    break;
+                case 'View All Department':
+                    perform.viewDepartments();
+                    break;
+                case 'Add Department':
+                    break;
+                case 'Quit':
+                    console.log('Goodbye!');
+                    process.exit();
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
 
-app.use((req, res) => {
-    res.status(404).end();
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+init();
